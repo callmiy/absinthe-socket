2a92760cf565bb0d8fa168dead37588164397b7d
Kanmii <maneptha@gmail.com>
2020-04-09 22:25:12 +0200

    feat: return error as string or object.

--- packages/socket/src/pushRequestUsing.js
+++ packages/socket/src/pushRequestUsing.js
@@ -27,7 +27,8 @@ const setNotifierRequestStatusSending = (absintheSocket, notifier) =>
     requestStatus: requestStatuses.sending
   });
 
-const createRequestError = message => new Error(`request: ${message}`);
+const createRequestError = message =>
+  new Error(typeof message === "string" ? message : JSON.stringify(message));
 
 const onTimeout = (absintheSocket, notifier) =>
   notifierNotifyActive(
